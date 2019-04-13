2f9bcdd84866224025c8ca7ef8d5a441932e8d9f
Louis Vanier <louis.vanier@shopify.com>
2019-04-13 08:49:20 -0400

    skeleton react-rails lobby

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  namespace :templates do
-    resources :matrix_users
-  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+     resources :templates_matrix_users, only: [:index, :create, :destroy, :update]
+    end
+  end
+
+  root to: 'lobby#index'
 end
