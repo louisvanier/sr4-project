c845926aea58b3e8bef41af1dd0135ddcfe161db
Louis Vanier <louis.vanier@shopify.com>
2021-03-20 11:27:54 -0400

    add minimal form validation and use as example in template matrix user new form

--- app/javascript/components/TemplateMatrixUserNew.tsx
+++ app/javascript/components/TemplateMatrixUserNew.tsx
@@ -1,71 +1,183 @@
 import * as React from "react"
 import {useInput} from '../hooks/useInput'
+import * as yup from 'yup';
+import { ErrorMessage, Field, Formik, Form } from 'formik'
 
 export function TemplateMatrixUserNew(props: any) {
-  const { value:name, bind:bindName, reset:resetName } = useInput('');
-  const { value:reaction, bind:bindReaction, reset:resetReaction } = useInput('');
-  const { value:intuition, bind:bindIntuition, reset:resetIntuition } = useInput('');
-  const { value:logic, bind:bindLogic, reset:resetLogic } = useInput('');
-  const { value:willpower, bind:bindWillpower, reset:resetWillpower } = useInput('');
-  const { value:computer, bind:bindComputer, reset:resetComputer } = useInput('');
-  const { value:cybercombat, bind:bindCybercombat, reset:resetCybercombat } = useInput('');
-  const { value:datasearch, bind:bindDatasearch, reset:resetDatasearch } = useInput('');
-  const { value:electronicwarfare, bind:bindElectronicwarfare, reset:resetElectronicwarfare } = useInput('');
-  const { value:hacking, bind:bindHacking, reset:resetHacking } = useInput('');
+  let schema = yup.object().shape({
+    name: yup.string().required(),
+    reaction: yup.number().integer().min(1).max(9).required(),
+    intuition: yup.number().integer().min(1).max(9).required(),
+    logic: yup.number().integer().min(1).max(9).required(),
+    willpower: yup.number().integer().min(1).max(9).required(),
+    computer: yup.number().integer().min(0).max(7).required(),
+    cybercombat: yup.number().integer().min(0).max(7).required(),
+    datasearch: yup.number().integer().min(0).max(7).required(),
+    electronicwarfare: yup.number().integer().min(0).max(7).required(),
+    hacking: yup.number().integer().min(0).max(7).required(),
+  });
 
-  const handleSubmit = () => {
-    console.log(name, reaction, intuition, logic, willpower, computer, cybercombat, datasearch, electronicwarfare, hacking);
+  const initialValues = {
+    name: '',
+    reaction: 1,
+    intuition: 1,
+    logic: 1,
+    willpower: 1,
+    computer: 0,
+    cybercombat: 0,
+    datasearch: 0,
+    electronicwarfare: 0,
+    hacking: 0
   }
 
   return (
-    <div className="row">
-      <div className="col-sm">
-        <label>Name</label>
-        <input type="text" {...bindName}></input>
-      </div>
-      <div className="col-sm">
-        <h3>Attributes</h3>
-        <div className="form-group">
-          <label className="col-sm-4 control-label">Reaction</label>
-          <input type="number" min="1" max="9" step="1" defaultValue="1"  {...bindReaction}></input>
-        </div>
-        <div className="form-group control-label">
-          <label className="col-sm-4">Intuition</label>
-          <input type="number" min="1" max="9" step="1" defaultValue="1" {...bindIntuition}></input>
-        </div>
-        <div className="form-group control-label">
-          <label className="col-sm-4">Logic</label>
-          <input type="number" min="1" max="9" step="1" defaultValue="1" {...bindLogic}></input>
-        </div>
-        <div className="form-group control-label">
-          <label className="col-sm-4">Willpower</label>
-          <input type="number" min="1" max="9" step="1" defaultValue="1" {...bindWillpower}></input>
-        </div>
-      </div>
-      <div className="col-sm">
-        <h3>Skills</h3>
-        <div className="form-group">
-          <label className="col-sm-6 control-label">Computer</label>
-          <input type="number" min="1" max="6" step="1" defaultValue="1" {...bindComputer}></input>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-6 control-label">Cybercombat</label>
-          <input type="number" min="1" max="6" step="1" defaultValue="1" {...bindCybercombat}></input>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-6 control-label">Data search</label>
-          <input type="number" min="1" max="6" step="1" defaultValue="1" {...bindDatasearch}></input>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-6 control-label">Electronic Warfare</label>
-          <input type="number" min="1" max="6" step="1" defaultValue="1" {...bindElectronicwarfare}></input>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-6 control-label">Hacking</label>
-          <input type="number" min="1" max="6" step="1" defaultValue="1" {...bindHacking}></input>
-        </div>
-      </div>
-      <button onClick={handleSubmit}>create</button>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(formik) => {
+        const { errors, touched, isValid, dirty } = formik;
+        return (
+          <Form>
+            <div className="container row">
+              <div className="col-sm">
+                <label>Name</label>
+                <Field
+                  type="text"
+                  name="name"
+                  id="name"
+                  className={errors.name && touched.name ? 'input-error' : null}
+                />
+                <ErrorMessage name="name" component="span" className="error" />
+              </div>
+              <div className="col-sm">
+                <h3>Attributes</h3>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Reaction</label>
+                  <Field
+                    type="number"
+                    name="reaction"
+                    id="reaction"
+                    min="1"
+                    max="9"
+                    className={errors.reaction && touched.reaction ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="reaction" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Intuition</label>
+                  <Field
+                    type="number"
+                    name="intuition"
+                    id="intuition"
+                    min="1"
+                    max="9"
+                    className={errors.intuition && touched.intuition ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="intuition" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Logic</label>
+                  <Field
+                    type="number"
+                    name="logic"
+                    id="logic"
+                    min="1"
+                    max="9"
+                    className={errors.logic && touched.logic ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="logic" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Willpower</label>
+                  <Field
+                    type="number"
+                    name="willpower"
+                    id="willpower"
+                    min="1"
+                    max="9"
+                    className={errors.willpower && touched.willpower ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="willpower" component="span" className="error" />
+                </div>
+              </div>
+              <div className="col-sm">
+                <h3>Skills</h3>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Computer</label>
+                  <Field
+                    type="number"
+                    name="computer"
+                    id="computer"
+                    min="0"
+                    max="7"
+                    className={errors.computer && touched.computer ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="computer" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Cybercombat</label>
+                  <Field
+                    type="number"
+                    name="cybercombat"
+                    id="cybercombat"
+                    min="0"
+                    max="7"
+                    className={errors.cybercombat && touched.cybercombat ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="cybercombat" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Data search</label>
+                  <Field
+                    type="number"
+                    name="datasearch"
+                    id="datasearch"
+                    min="0"
+                    max="7"
+                    className={errors.datasearch && touched.datasearch ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="datasearch" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Electronic warfare</label>
+                  <Field
+                    type="number"
+                    name="electronicwarfare"
+                    id="electronicwarfare"
+                    min="0"
+                    max="7"
+                    className={errors.electronicwarfare && touched.electronicwarfare ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="electronicwarfare" component="span" className="error" />
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-4 control-label">Hacking</label>
+                  <Field
+                    type="number"
+                    name="hacking"
+                    id="hacking"
+                    min="0"
+                    max="7"
+                    className={errors.hacking && touched.hacking ? 'input-error' : null}
+                  />
+                  <ErrorMessage name="hacking" component="span" className="error" />
+                </div>
+              </div>
+              <button
+                  type="submit"
+                  className={!(dirty && isValid) ? "disabled-btn" : ""}
+                  disabled={!(dirty && isValid)}
+                >
+                  Create
+                </button>
+            </div>
+          </Form>
+        )
+      }}
+    </Formik>
   )
 };
